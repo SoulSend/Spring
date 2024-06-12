acdab7b554bc84ecf30230daf197abe2a31303a3
HRC <hrc_2525@qq.com>
2024-06-12 20:49:30 +0800

    aop动态代理学习完毕代码提交，同时完成readme文档Aop编程思想的编写

--- spring-aop-annotation-06/src/test/java/AopTest.java
+++ spring-aop-annotation-06/src/test/java/AopTest.java
@@ -1,3 +1,5 @@
+import com.hrc.dynamicproxy.ProxyFactory;
+import com.hrc.staticproxy.Calculator;
 import com.hrc.staticproxy.CalculatorImp;
 import com.hrc.staticproxy.CalculatorProxy;
 import org.junit.jupiter.api.Test;
@@ -8,5 +10,12 @@ public class AopTest {
         CalculatorImp calculatorImp=new CalculatorImp();
         CalculatorProxy proxy=new CalculatorProxy(calculatorImp);
         int sum = proxy.add(1, 2);
+    }
+    @Test
+    public void dynamicProxyTest(){
+        ProxyFactory factory = new ProxyFactory(new CalculatorImp());
+        Calculator proxy = (Calculator) factory.getProxy();
+        proxy.add(1,0);
+        //proxy.div(1,1);
     }
 }
